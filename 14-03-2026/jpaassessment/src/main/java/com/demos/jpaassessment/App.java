e041fbdb06cfd50caf91dab28b833690ba8b691d
cipher-sphere <shivam0hapur@gmail.com>
2026-03-14 12:51:06 +0530

    done with test

---
+++ 14-03-2026/jpaassessment/src/main/java/com/demos/jpaassessment/App.java
@@ -0,0 +1,10 @@
+package com.demos.jpaassessment;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
